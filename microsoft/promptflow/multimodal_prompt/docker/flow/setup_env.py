aea6f2eae2f464e553f3fe61c42fad9a4c16d85c
AWS_user-at-317609140281 <olonok@gmail.com>
2024-12-18 13:28:10 +0000

    pf visual classifier

---
+++ microsoft/promptflow/multimodal_prompt/docker/flow/setup_env.py
@@ -0,0 +1,30 @@
+import os
+from typing import Union
+
+from promptflow.core import tool
+from promptflow.connections import AzureOpenAIConnection, OpenAIConnection
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+@tool
+def setup_env(connection: Union[AzureOpenAIConnection, OpenAIConnection], config: dict):
+    if not connection or not config:
+        return
+
+    if isinstance(connection, AzureOpenAIConnection):
+        os.environ["OPENAI_API_TYPE"] = "azure"
+        os.environ["OPENAI_API_BASE"] = connection.api_base
+        os.environ["OPENAI_API_KEY"] = connection.api_key
+        os.environ["OPENAI_API_VERSION"] = connection.api_version
+
+    if isinstance(connection, OpenAIConnection):
+        os.environ["OPENAI_API_KEY"] = connection.api_key
+        if connection.organization is not None:
+            os.environ["OPENAI_ORG_ID"] = connection.organization
+
+    for key in config:
+        os.environ[key] = str(config[key])
+
+    return "Ready"
