726e6d9aaea65bd37a0671b7a9d32f2fb1a59ba6
AWS_user-at-317609140281 <olonok@gmail.com>
2024-12-19 19:13:14 +0000

    PF visual classifier

--- microsoft/promptflow/multimodal_prompt/get_image_analyze.py
+++ microsoft/promptflow/multimodal_prompt/get_image_analyze.py
@@ -1,6 +1,7 @@
 from promptflow.core import tool
 import base64, requests
 import os
+from rapidocr_onnxruntime import RapidOCR
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -18,13 +19,23 @@ def encode_image_to_base64(image_path):
         base64_encoded_data = base64.b64encode(image_data)
         # Convert the Base64 bytes to a string
         base64_string = base64_encoded_data.decode("utf-8")
-    return "data:image/tif;base64," + base64_string
+    return "data:image/png;base64," + base64_string
+
+
+def extract_text(image_path, engine):
+    result, elapse = engine(image_path)
+    content = ""
+    for r in result:
+        content = content + " " + r[1]
+    return content
 
 
 @tool
 def get_image_analyze(path: str):
     """
     Returns a list of example images and their categories."""
-    image = encode_image_to_base64(os.path.join(ROOT_DIR, path))
+    img_path = os.path.join(ROOT_DIR, path)
+
+    image = encode_image_to_base64(image_path=img_path)
 
     return {"path": path, "image": image}
